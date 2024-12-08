5d146117d8d7bc430ac0ccaa826afec1ce516055
Rohith K <skrishrkat7@gmail.com>
2024-12-08 13:45:14 -0600

    Update ListingServiceImpl.java

--- foodforward/src/main/java/com/schemax/foodforward/service/impl/ListingServiceImpl.java
+++ foodforward/src/main/java/com/schemax/foodforward/service/impl/ListingServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.schemax.foodforward.dto.CreateListingDTO;
 import com.schemax.foodforward.dto.ListingSearchDTO;
 import com.schemax.foodforward.dto.UpdateListingDTO;
+import com.schemax.foodforward.dto.ListingDTO;
 import com.schemax.foodforward.model.Item;
 import com.schemax.foodforward.model.Listing;
 import com.schemax.foodforward.repository.DonorRepository;
@@ -30,8 +31,8 @@ public class ListingServiceImpl implements ListingService {
     private ItemRepository itemRepository;
 
     @Override
-    public List<Listing> searchListings(ListingSearchDTO searchDTO) {
-        List<Listing> listings = listingRepository.findListingsWithFilters(
+    public List<ListingDTO> searchListings(ListingSearchDTO searchDTO) {
+        List<ListingDTO> listings = listingRepository.findListingsWithFilters(
                 searchDTO.getFoodType(),
                 searchDTO.getQuantityNeeded(),
                 searchDTO.getExpiryDate(),
